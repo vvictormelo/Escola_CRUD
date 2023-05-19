257c713540d45a0291bb215c6f108952fa9a4203
Victor Melo <melovictor515@gmail.com>
2023-05-18 23:26:30 -0300

    Implementação do menu e case das ações

---
+++ .venv/entities.py
@@ -0,0 +1,19 @@
+class TipoCurso:
+    def __init__(self, descricao):
+        self.descricao = descricao
+
+
+class Titulo:
+    def __init__(self, descricao):
+        self.descricao = descricao
+
+
+class Instituicao:
+    def __init__(self, sigla, descricao):
+        self.sigla = sigla
+        self.descricao = descricao
+
+
+class TipoDisciplina:
+    def __init__(self, descricao):
+        self.descricao = descricao
--- .venv/main.py
+++ .venv/main.py
@@ -31,6 +31,8 @@ def create_tables():
                         descricao TEXT)''')
 
         conexao.commit()
+
+        print("\033[1;36m\nTabelas criadas com êxito.\033[m")
     except ConnectionError as e:
         print("Erro no banco", e)
 
@@ -132,3 +134,59 @@ def insert_tipo_disciplina():
     finally:
         cursor.close()
         conexao.close
+
+
+def menu():
+
+    print("\n")
+    print("-=" * 15)
+    print(f'\033[1;36m{"MENU - CADASTRO ACADÊMICO":>27}\033[m')
+    print("-=" * 15)
+    print("\n1 - Criar tabelas")
+    print(
+        """2 - Crud entidades: 
+    2.1 - Criar Tipo de curso
+    2.2 - Criar Titulo 
+    2.3 - Criar Instituição
+    2.4 - Criar Tipo de Disciplina"""
+    )
+    print("3 - Sair")
+
+
+def program():
+
+    print("\033[1;36m\nSEJA BEM VINDO...\n\033[m")
+
+    acao = 0
+
+    try:
+
+        while acao != 3:
+
+            menu()
+            acao = float(input("\nInsira a ação desejada: "))
+
+            match acao:
+                case 1:
+                    create_tables()
+                case 2.1:
+                    insert_tipo_curso()
+                case 2.2:
+                    insert_titulo()
+                case 2.3:
+                    insert_instituicao()
+                case 2.4:
+                    insert_tipo_disciplina()
+                case 3:
+                    acao = 3
+                    print("Até logo! Saindo..")
+            if (acao > 4) or (acao < 1):
+                print(
+                    "\033[1;31m\nOops...Ação inválida. Tente novamente!\n\033[m")
+
+    except ValueError:
+        print("\033[1;31m\nOops...Ação inválida. Tente novamente!\n\033[m")
+
+
+if __name__ == "__main__":
+    program()
